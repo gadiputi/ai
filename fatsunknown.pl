786562de6211343d22f3b2e4f3c8ad847fb07672
gadiputi <192425069.simats@saveetha.com>
2026-02-18 11:01:43 +0530

    Add files via upload

---
+++ fatsunknown.pl
@@ -0,0 +1,8 @@
+% --- Facts ---
+colour(cherry, red).
+colour(banana, yellow).
+colour(apple, red).
+colour(apple, green).
+colour(orange, orange).
+% Default rule (if no color is found)
+colour(X, unknown).
